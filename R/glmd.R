90ebb7d21b7cd50b6c0419094c333b07c4f33f05
adamiani <athos.damiani@gmail.com>
2014-02-20 12:48:18 -0300

    First commit

---
+++ R/glmd.R
@@ -0,0 +1,13 @@
+#' Faz diag do giba 
+#'
+#' \code{gmld} Faz um diagnóstico completo de um objeto 'glm'. 
+#'
+#' @param model um objeto, da classe 'glm' ou 'dglm' a ser diagnosticado.
+#' @param ... outros argumentos passados para métodos específicos.
+#' @return um objeto gmld
+#' @export
+#' @seealso \code{\link{glm.diag}} and \code{\link{glm.diag.plot}}
+glmd <- function(model, ...) {
+  # faz diag do giba...
+  return("Diag do giba!")
+}

94094e31fdf42d3d9ece9523787d24b9193d3076
adamiani <athos.damiani@gmail.com>
2014-02-24 01:00:09 -0300

    doc teste do glmd

--- R/glmd.R
+++ R/glmd.R
@@ -3,11 +3,12 @@
 #' \code{gmld} Faz um diagnóstico completo de um objeto 'glm'. 
 #'
 #' @param model um objeto, da classe 'glm' ou 'dglm' a ser diagnosticado.
+#' @param ggplot um booleano. Se falso (padrão), os gráficos são criados pela função plot. Caso contrário, sao geradas versões ggplot.
 #' @param ... outros argumentos passados para métodos específicos.
 #' @return um objeto gmld
 #' @export
 #' @seealso \code{\link{glm.diag}} and \code{\link{glm.diag.plot}}
-glmd <- function(model, ...) {
+glmd <- function(model, ggplot=TRUE, ...) {
   # faz diag do giba...
   return("Diag do giba!")
 }
